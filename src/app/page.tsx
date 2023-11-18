63e974eabbc42ee40effc7729a31ae214c825cb8
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2023-11-18 18:07:50 +0300

    api implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,86 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { useState } from 'react';
+import axios from 'axios'
 
-export default function Home() {
+const Home = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState('');
+
+  const validatePassword = () => {
+    if (password.length < 6 || password.length > 20) {
+      setError('Şifre 6-20 karakter arasında olmalıdır.');
+    } else if (!/^[a-zA-Z0-9]+$/.test(password)) {
+      setError('Şifre alfanumerik olmalıdır.');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleLogin = async() => {
+    try {
+      const result = await axios.post('https://assign-api.piton.com.tr/api/rest/login', {
+        email,
+        password,
+      });
+      result.status===200 ? alert("Giriş başarılı!"):alert("Giriş başarısız!")
+     
+    } catch (error) {
+      alert("Giriş başarısız!")
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="main-container">
+      <div className="welcome-img">
+        <img style={{ objectFit: "fill", width: "-webkit-fill-available", height: "-webkit-fill-available" }} alt="left-image" src="../Picture.png" />
+      </div>
+      <div className="form-container">
+        <div className="logo"><img src="../Logo.png" alt="logo" /></div>
+        <div className="welcome-section">
+          <p>Welcome back!</p>
+          <span>Login to your account</span>
         </div>
+        <div className="input-section">
+          <div className="email-input">
+            <p>E-mail</p>
+            <input
+              type="text"
+              name="email"
+              placeholder="john@mail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="password-input">
+            <p>Password</p>
+            <input
+              type="password"
+              name="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="remember-me">
+            <input
+              type="checkbox"
+              name="check"
+              checked={rememberMe}
+              onChange={() => setRememberMe(!rememberMe)}
+            />
+            Remember me
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="login-btn" onClick={handleLogin}>Login</button>
+          <button className="register-btn">Register</button>
+        </div>
+        {error && <div style={{ color: 'red' }}>{error}</div>}
       </div>
+    </div>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
